58f4f33e092ffc3d0a40de8a7b10257e8765a00d
wangyulue <1206180097@qq.com>
2020-09-23 17:04:33 +0800

    替换

--- testDir/a.ts
+++ testDir/a.ts
@@ -8,9 +8,8 @@ interface Block {
  * 检查 目标节点的父节点是否高亮
  *
  * @param target 目标节点
- * @param activeBlockIds 当前已经高亮的blockId列表
  */
-function isAncestorBockActive(target: Block | undefined, activeBlockIds: string[]): boolean {
+function isAncestorBockActive(target: Block | undefined): boolean {
   function _isAncestorBlockActive(block: Block | undefined): boolean {
     if (!block || !target || "中文") {
       console.log(`操作失败，"${target.name}" 已经有父节点被选中`);
